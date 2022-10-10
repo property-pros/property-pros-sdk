1ecd03ed048c91f7174ec7041c9f0f13fca4da18
Scott Terry <scottterry@MacBook-Pro-2.local>
2022-10-10 16:59:59 -0700

    try regular export for type declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,2 @@
-declare module "property-pros-sdk/auth" {
-    export * from "./generated/auth/auth"
-}
-
-declare module "property-pros-sdk/notePurchaseAgreement" {
-    export * from "./generated/notepurchaseagreement/notePurchaseAgreement"
-}
+export * as auth from "./generated/auth/auth";
+export * as notepurchaseagreement from "./generated/notepurchaseagreement/notepurchaseagreement";
