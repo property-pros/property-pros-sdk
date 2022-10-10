b0da49b15a1142e25c6ced31fbf10e28f5b27609
Scott Terry <scottterry@MacBook-Pro-2.local>
2022-10-10 16:45:33 -0700

    attempt to fix type module resolution

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module "property-pros-sdk/auth" {
+    export * from "./generated/auth/auth"
+}
+
+declare module "property-pros-sdk/notePurchaseAgreement" {
+    export * from "./generated/notepurchaseagreement/notePurchaseAgreement"
+}
